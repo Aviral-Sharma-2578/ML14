2cb7fc3700dba7547781a9ab92ea7431e8565fba
Aviral-Sharma-2578 <117987884+Aviral-Sharma-2578@users.noreply.github.com>
2023-07-25 09:07:51 +0530

    Update train.py

--- train.py
+++ train.py
@@ -19,23 +19,22 @@ def get_args():
     parser.add_argument("--world", type=int, default=1)
     parser.add_argument("--stage", type=int, default=1)
     parser.add_argument("--action_type", type=str, default="simple")
-    parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--gamma', type=float, default=0.9, help='discount factor for rewards')
     parser.add_argument('--tau', type=float, default=1.0, help='parameter for GAE')
     parser.add_argument('--beta', type=float, default=0.01, help='entropy coefficient')
+    parser.add_argument('--lr', type=float, default=1e-6)
+    parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--epsilon', type=float, default=0.2, help='parameter for Clipped Surrogate Objective')
     parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument('--num_epochs', type=int, default=10)
-    parser.add_argument("--num_local_steps", type=int, default=512)
+    parser.add_argument("--num_local_steps", type=int, default=5)
     parser.add_argument("--num_global_steps", type=int, default=5)
     parser.add_argument("--num_processes", type=int, default=8)
     parser.add_argument("--save_interval", type=int, default=50, help="Number of steps between savings")
+    parser.add_argument("--saved_path", type=str, default="trained_models")
     parser.add_argument("--max_actions", type=int, default=200, help="Maximum repetition steps in test phase")
     parser.add_argument("--log_path", type=str, default="tensorboard/ppo_super_mario_bros")
-    parser.add_argument("--saved_path", type=str, default="trained_models")
     args = parser.parse_args()
     return args
-
 
 def train(opt):
     if torch.cuda.is_available():
@@ -47,27 +46,18 @@ def train(opt):
     os.makedirs(opt.log_path)
     if not os.path.isdir(opt.saved_path):
         os.makedirs(opt.saved_path)
-    mp = _mp.get_context("spawn")
+
+    # Remove the use of multiprocessing and use a basic single channel (queue.Queue)
     envs = MultipleEnvironments(opt.world, opt.stage, opt.action_type, opt.num_processes)
+
     model = PPO(envs.num_states, envs.num_actions)
-    if torch.cuda.is_available():
-        model.cuda()
     model.share_memory()
-    process = mp.Process(target=eval, args=(opt, model, envs.num_states, envs.num_actions))
-    process.start()
+
+    # The optimizer is created using the model's parameters
     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
-    [agent_conn.send(("reset", None)) for agent_conn in envs.agent_conns]
-    curr_states = [agent_conn.recv() for agent_conn in envs.agent_conns]
-    curr_states = torch.from_numpy(np.concatenate(curr_states, 0))
-    if torch.cuda.is_available():
-        curr_states = curr_states.cuda()
+
     curr_episode = 0
-    while True:
-        # if curr_episode % opt.save_interval == 0 and curr_episode > 0:
-        #     torch.save(model.state_dict(),
-        #                "{}/ppo_super_mario_bros_{}_{}".format(opt.saved_path, opt.world, opt.stage))
-        #     torch.save(model.state_dict(),
-        #                "{}/ppo_super_mario_bros_{}_{}_{}".format(opt.saved_path, opt.world, opt.stage, curr_episode))
+    for fr in range(1000):
         curr_episode += 1
         old_log_policies = []
         actions = []
@@ -85,12 +75,11 @@ def train(opt):
             actions.append(action)
             old_log_policy = old_m.log_prob(action)
             old_log_policies.append(old_log_policy)
-            if torch.cuda.is_available():
-                [agent_conn.send(("step", act)) for agent_conn, act in zip(envs.agent_conns, action.cpu())]
-            else:
-                [agent_conn.send(("step", act)) for agent_conn, act in zip(envs.agent_conns, action)]
 
-            state, reward, done, info = zip(*[agent_conn.recv() for agent_conn in envs.agent_conns])
+            for agent_queue, act in zip(envs.agent_queues, action.cpu()):
+                agent_queue.put(("step", act))
+
+            state, reward, done, info = zip(*[agent_queue.get() for agent_queue in envs.agent_queues])
             state = torch.from_numpy(np.concatenate(state, 0))
             if torch.cuda.is_available():
                 state = state.cuda()
@@ -134,7 +123,6 @@ def train(opt):
                                                    torch.clamp(ratio, 1.0 - opt.epsilon, 1.0 + opt.epsilon) *
                                                    advantages[
                                                        batch_indices]))
-                # critic_loss = torch.mean((R[batch_indices] - value) ** 2) / 2
                 critic_loss = F.smooth_l1_loss(R[batch_indices], value.squeeze())
                 entropy_loss = torch.mean(new_m.entropy())
                 total_loss = actor_loss + critic_loss - opt.beta * entropy_loss
@@ -143,6 +131,12 @@ def train(opt):
                 torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
                 optimizer.step()
         print("Episode: {}. Total loss: {}".format(curr_episode, total_loss))
+
+
+# Example usage:
+# opt = ...  # Define your training options
+# train(opt)
+
 
 
 if __name__ == "__main__":
